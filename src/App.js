fb15a80f9e7b0ebe8aa1af428a9cd114424b4802
Park <park@Parkui-MacBookPro.local>
2020-10-01 22:32:34 +0900

    Dynamic component error fixed

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ function Food ({ name,picture }) {
 return (
   <div>
     <h1> I Like {name} </h1>
-    <img src={picture}/>
+    <img src={picture} alt={name}/>
   </div>
 )}
 
 const FoodLike =[
   {
+    id : "1",
     name : "치킨",
     image : "https://economychosun.com/query/upload/303/20190608214338_gubchoja.jpg"
   },
   {
+    id : "2",
     name : "피자",
     image : "https://lh3.googleusercontent.com/proxy/bJo5OScW46wjUeJS80Jk5NiU7a1usTnJsCDMIhALrzvldKP-AOywxiAz3qa03cXx9ZEqZgu9I1Gd2T7dqoeL1aLGHY2v7soJsc7fEWefR3AqceI"
   },
   {
+    id : "3",
     name : "햄버거",
     image : "https://img.hankyung.com/photo/201908/01.20330262.1.jpg"
   },
   {
+    id : "4",
     name : "닭갈비",
     image : "https://contents.lotteon.com/itemimage/LD/15/06/46/43/6_/0/LD150646436_0_2.jpg/dims/resizef/824X824"
   }
@@ -32,7 +36,7 @@ function App() {
   return (
     <div>
       {
-        FoodLike.map(foodname => (<Food name={foodname.name} picture={foodname.image} />))
+        FoodLike.map(foodname => (<Food key={foodname.id} name={foodname.name} picture={foodname.image} />))
       }
     </div>
   );
