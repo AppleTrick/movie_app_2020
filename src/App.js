1659495e1634da98b3c4eee3571a2185a5f5bbbb
Park <arrans9591@gmail.com>
2020-10-11 21:07:00 +0900

    movie component planing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 
 class App extends React.Component{
-
+  state ={
+    isLoading : true ,
+    movie : []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    }, 6000);
+  }
   render(){
-    return <div></div>;
+    const {isLoading} = this.state;
+    return (
+    <div> {isLoading ? "Loading..." : "We are ready"} </div>
+    );
   }
 }
 
