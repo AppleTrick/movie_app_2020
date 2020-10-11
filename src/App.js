8ce2a8c949c8e9b4eaf71439589ee936e93b859d
Park <arrans9591@gmail.com>
2020-10-11 20:37:30 +0900

    reset react Movie Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 
 class App extends React.Component{
-  state ={
-    count : 0
-  }
-  add = () => {
-    this.setState(current => ({count : current.count + 1}));
-  }
-  minus = () => {
-    this.setState(noma => ({count : noma.count - 1}));
-  }
 
   render(){
-    return (
-    <div>
-      <h1>the number is {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>minus</button>
-    </div>
-    );
+    return <div></div>;
   }
 }
 
